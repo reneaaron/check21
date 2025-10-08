4e390d87b25006a80f560665779283d46b5d06a3
René Aaron <rene@twentyuno.net>
2025-10-08 21:41:47 +0200

    feat: init shadcn project structure

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <Button>Click me</Button>
+      <div className='bg-red-500'>
+        asdf
+      </div>
+    </>
+  )
+}
+
+export default App
